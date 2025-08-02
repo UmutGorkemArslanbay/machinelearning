12c4a39d4bf0446d73b6b778728674b703459778
Umut Görkem Arslanbay <umtgrkm2002@gmail.com>
2025-08-02 15:56:06 +0300

    Linear Regresion nedir ?

---
+++ 2-Supervised Learning/2.1-Linear Regression/code.py
@@ -0,0 +1,56 @@
+# Elimizdeki dataset Boston.csv
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+def main():
+    
+    # Datayı ilk başta okuyalım
+    df = pd.read_csv(r"C:\Users\umut\Desktop\video\ML_Tutorial\2-Supervised Learning\2.1-Linear Regression\data\boston.csv", index_col=0)
+    # print(df.head(5)) bunu etkinleştirerek okuyabilirsiniz veya csv'yi açıp bakabilirsiniz.
+    
+    X = df.drop("medv", axis=1) # medv sütununu kaldırdı
+    y = df["medv"] # medv sütunun y'ye taşıdı.
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    model = LinearRegression()
+    model.fit(X_train,y_train)
+    
+
+    y_pred = model.predict(X_test)
+
+    mse = mean_squared_error(y_test,y_pred)
+    r2 = r2_score(y_test,y_pred)
+
+    print(f"Linear Regression sonuçları:")
+    print(f"  • MSE: {mse:.2f}")
+    print(f"  • R² : {r2:.2f}\n")
+    
+    # Burada tahmin etmeye çalıştığımız şey, fiyat ve pozitif katsayılar fiyatı arttırıcı, negatifler azaltıcı bir etkiye sahiptir.
+    coeffs = pd.Series(model.coef_, index=X.columns)
+    print("Özellik katsayıları (en yüksekten en düşüğe):")
+    print(coeffs.sort_values(ascending=False))
+    
+    # Bunu grafiğe dökecek olursak
+    plt.figure(figsize=(6,6))
+    plt.scatter(y_test,y_pred,edgecolors="k")
+    plt.plot([y.min(), y.max()], [y.min(), y.max()], 'r--')
+    plt.xlabel("Gerçek medv")
+    plt.ylabel("Tahmin medv")
+    plt.title("Real vs Predict")
+    plt.tight_layout()
+    plt.show()
+
+
+    # Fotoğrafdaki kırmızı çizgi x ve y eşitlendiğinde noktalar bu çizgi üzerinde olur.
+    # Model ne kadar iyiyse noktalar bu çizgi etrafında yoğunlaşır.
+    # Model orta kısımlarda iyi bir sonuç göstermesine rağmen uçlardaki varyansyonları iyi yakalayamamış.
+    # Bu hatayı gidermek için daha farklı yöntemler kullanılabilir.
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ 2-Supervised Learning/2.1-Linear Regression/tanim.md
@@ -0,0 +1,14 @@
+# Linear Regression
+ Bir veya birden fazla bağımsız değişkenin bir bağımlı değişkeni nicel olarak tahmin etmek için kullanılan yöntemdir.
+ - Fiyat Tahmini
+ - Satış Projeksiyonu
+ - Sıcaklık/Hava Durumu Tahmini
+# R2 Score
+ Hata indirgeme yüzdesidir. 
+ 1.0 -> Mükemmel Uyum, model verinin genel akışını yakaladı
+ 0.8 -> %80 variance açıklanmış
+ 0.0 -> Ortalama tahminden daha iyi değil.
+ Negatif -> Ortalama tahminden bile kötü
+# Coeffs
+ Bir hesaplamadır.Bu hesaplama ilgili feature'ın tahmin değerine etkisini gösterir.
+ Katsayıların mutlak büyüklüğüne göre sıralamak, hangi değişkenlerin en güçlü etkiye sahip olduğunu görmemizi sağlar.
